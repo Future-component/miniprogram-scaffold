28207291dca1fd55da46aa249ce4b3a20cf4054c
BETH-zhang <522718277@qq.com>
2020-03-11 12:25:47 +0800

    添加骨架屏组件

---
+++ components/skeleton/index.js
@@ -0,0 +1,25 @@
+const app = getApp()
+Component({
+  options: {
+    addGlobalClass: true,
+  },
+  
+  properties: {
+    active: {
+      type: String,
+      value: 'active',
+    },
+  },
+
+  data: {
+  },
+
+  attached() {
+  },
+
+  ready() {
+  },
+
+  methods: {
+  }
+})
